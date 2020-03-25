18fc5e134bdb30700c2ebfdba0c0cce80d99aba3
voracious-semicolon-2550 <mfh.haider@gmail.com>
2020-03-25 23:32:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(number)
   start = 2
   if number > 1
     range = (start..number-1).to_a
-    range.none? do |num_to_test|  none? to return true. If none of the numbers
+    range.none? do |num_to_test| 
                                 # return true, then they are not divisible by the number and the number is prime
       number % num_to_test == 0
     end
